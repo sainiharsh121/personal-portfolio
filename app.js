11e1238d4f693b54c15f1fbfe50d2447fc3b85a4
sainiharsh121 <Harshsaini032004@gmail.com>
2025-06-22 20:46:40 +0530

    added a log

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+console.log("my site is running");
 const video1 = document.getElementById('projectVideo1');
 const video2 = document.getElementById('projectVideo2');
 const video3 = document.getElementById('projectVideo3');
